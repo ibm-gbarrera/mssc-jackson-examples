5018489ce27790856abeed51209ecf3036ab278b
GUILLERMO ALEJANDRO BARRERA GRANADOS <gbarrera@mx1.ibm.com>
2024-08-23 17:23:19 -0600

    Property Naming Strategy: Kebab

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
@@ -0,0 +1,27 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ActiveProfiles("kebab")
+@JsonTest
+public class BeerDtoKebabTest extends BaseTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testKebab() throws JsonProcessingException {
+        BeerDto dto = getDto();
+
+        String json = objectMapper.writeValueAsString(dto);
+
+        System.out.println(json);
+    }
+    
+}
